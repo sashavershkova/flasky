85ec00ef17286b1c45b08b8259cfd74c43122ba4
sashavershkova <a.m.vershkova@gmail.com>
2025-04-26 14:18:37 -0700

    add read, update, delete one cat

--- app/routes/cats_routes.py
+++ app/routes/cats_routes.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.cats import Cat
 from ..db import db
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix = "/cats")
 
+# POST ONE CAT
 @cats_bp.post("")
 def add_new_cat():
     request_body = request.get_json()
@@ -24,6 +25,7 @@ def add_new_cat():
 
     return response_new_cat, 201
 
+# GET ALL CATS
 @cats_bp.get("")
 def get_all_cats():
     query = db.select(Cat).order_by(Cat.id)
@@ -40,27 +42,57 @@ def get_all_cats():
         
     return response_cats
 
-# @cats_bp.get("/<id>")
-# def get_one_cat(id):
-#     cat = validate_cat(id)
-#     return dict(
-#         id = cat.id,
-#         name = cat.name,
-#         color = cat.color,
-#         breed = cat.breed
-#     )
+# GET ONE CAT
+@cats_bp.get("/<cat_id>")
+def get_one_cat(cat_id):
+    cat = validate_cat(cat_id)
 
-# def validate_cat(id):
-#     try:
-#         id = int(id)
-#     except:
-#         response = {"message": f"id {id} is invalid"}
-#         abort(make_response(response, 400))
+    return dict(
+        id = cat.id,
+        name = cat.name,
+        color = cat.color,
+        breed = cat.breed
+    )
 
-#     for cat in cats:
-#         if cat.id == id:
-#             return cat
+# UPDATE ONE CAT
+@cats_bp.put("/<cat_id>")
+def update_one_cat(cat_id):
+    cat = validate_cat(cat_id)
+    request_body = request.get_json()
+    cat.name = request_body["name"]
+    cat.color = request_body["color"]
+    cat.breed = request_body["breed"]
+
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+# DELETE ONE CAT
+@cats_bp.delete("/<cat_id>")
+def delete_one_cat(cat_id):
+    cat = validate_cat(cat_id)
+
+    db.session.delete(cat)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+
+# HELPER FUNCTION VALIDATING CAT
+def validate_cat(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except:
+        response = {"message": f"id {cat_id} is invalid"}
+        abort(make_response(response, 400))
+
+    query = db.select(Cat).where(Cat.id == cat_id)
+    cat = db.session.scalar(query)
     
-#     response = {"message": f"cat with id {id} not found"}
-#     abort(make_response(response, 404))
+    if not cat:
+        response = {"message": f"cat with id {cat_id} not found"}
+        abort(make_response(response, 404))
+
+    return cat
 
